14857475f8b4156c9b5f91130e195fec3bd261ec
justgoodin <rehan.haider.v3@gmail.com>
2021-07-01 10:27:11 +0000

    FEAT: Toggle between dark and light themese

--- design/alexis/static/js/app.js
+++ design/alexis/static/js/app.js
@@ -10,7 +10,25 @@ window.onload = function (e) {
     else {
         var pathArray = window.location.pathname.split('/');
         activePath = pathArray[1];
-        console.log(activePath);
     }
     document.getElementById(activePath).className += " active";
 }
+
+const themeMap = {
+    dark: "light",
+    light: "dark"
+};
+
+function toggleTheme() {
+    const bodyClass = document.body.classList;
+    theme = bodyClass[0].split("-")[0];
+    bodyClass.replace(bodyClass[0], themeMap[theme] + "-theme");
+    document.getElementById(theme).style.display = "flex";
+    document.getElementById(themeMap[theme]).style.display = "none";
+
+    console.log(theme);
+    console.log(themeMap[theme]);
+}
+
+document.getElementById("themeButton").onclick = toggleTheme;
+
