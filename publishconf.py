bd793e9d311d5901b9507d5fb4ba23f818cc6e75
justgoodin <rehan.haider.v3@gmail.com>
2021-07-04 23:29:19 +0000

    FIX: Added GTAG

--- publishconf.py
+++ publishconf.py
@@ -6,19 +6,20 @@
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://uberpython.com'
+SITEURL = "https://uberpython.com"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-GOOGLE_ANALYTICS = "G-2X08GQZGV7"
+# DISQUS_SITENAME = ""
+GTAG = "G-2X08GQZGV7"
