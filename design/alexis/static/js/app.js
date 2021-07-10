25511c77d7c5ee51cc57741df455d41c6d12b6d9
justgoodin <rehan.haider.v3@gmail.com>
2021-07-10 11:54:41 +0000

    FIX: Attemted light mode transition fix'

--- design/alexis/static/js/app.js
+++ design/alexis/static/js/app.js
@@ -43,44 +43,8 @@ function myInit() {
 };
 
 
-// Theme toggler
-var themeNext = {
-    dark: "light",
-    light: "dark"
-};
 
-if (localStorage.getItem('theme')) {
-    var theme = localStorage.getItem('theme');
-    document.body.classList.remove('dark-theme', 'light-theme');
-    document.body.classList.add(theme + '-theme');
-    document.getElementById(themeNext[theme]).style.display = "flex";
-    document.getElementById(theme).style.display = "none";
 
-}
-
-var switchTheme = document.getElementById('themeButton');
-var toggleLight = document.getElementById('light');
-var toggleDark = document.getElementById('dark');
-
-switchTheme.addEventListener('click', function (e) {
-    e.preventDefault();
-});
-toggleLight.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.body.classList.add('light-theme');
-    document.body.classList.remove('dark-theme');
-    localStorage.setItem('theme', 'light');
-    toggleLight.style.display = "none";
-    toggleDark.style.display = "flex";
-});
-toggleDark.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.body.classList.add('dark-theme');
-    document.body.classList.remove('light-theme');
-    localStorage.setItem('theme', 'dark');
-    toggleLight.style.display = "flex";
-    toggleDark.style.display = "none";
-});
 
 
 /**
