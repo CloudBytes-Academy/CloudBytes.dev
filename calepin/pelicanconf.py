09229b3e54ba1944c6c26c5085eb48ac0c2f26f8
justgoodin <rehan.haider.v3@gmail.com>
2021-07-23 18:28:02 +0000

    REMOVED: SEO Pluging setting

--- calepin/pelicanconf.py
+++ calepin/pelicanconf.py
@@ -95,12 +95,6 @@ SITEMAP = {
     "changefreqs": {"articles": "weekly", "indexes": "daily", "pages": "monthly"},
 }
 
-# SEO Settings
-SEO_REPORT = True  # To enable this feature
-SEO_ENHANCER = False  # To disable this feature
-SEO_ENHANCER_OPEN_GRAPH = False  # The default value for this feature
-SEO_ENHANCER_TWITTER_CARDS = False  # The default value for this feature
-
 MARKDOWN = {
     "extension_configs": {
         # Needed for code syntax highlighting
