5169d4ebb9d3a2d7fb5669f65b9fe8fc04761e3b
Rehan Haider <email@rehanhaider.com>
2021-10-22 15:50:00 +0000

    FIX S0019 404

--- plugins/search.py
+++ plugins/search.py
@@ -14,11 +14,13 @@ def main(generator):
     """
     Main function to configure and send data to Algolia
     """
+    logger.info("Searching for Algolia Credentials")
     ALGOLIA_INDEX_NAME = generator.settings.get("ALGOLIA_INDEX_NAME")
     ALGOLIA_APP_ID = generator.settings.get("ALGOLIA_APP_ID")
     ALGOLIA_ADMIN_API_KEY = generator.settings.get("ALGOLIA_ADMIN_API_KEY")
 
     if ALGOLIA_ADMIN_API_KEY and ALGOLIA_APP_ID and ALGOLIA_INDEX_NAME:
+        logger.info("Found Algolia credentials, proceeding to indexing...")
         client = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_ADMIN_API_KEY)
         index = client.init_index(ALGOLIA_INDEX_NAME)
 
@@ -35,6 +37,7 @@ def main(generator):
             records["category"] = article.category
             print("Adding Algolia object...")
             index.save_objects([records], {"autoGenerateObjectIDIfNotExist": True})
+        logger.info("Indexing complete...")
     else:
         logger.error("No Algolia Configuration Found: Skipping Algolia update")
 
