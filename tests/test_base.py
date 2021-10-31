8c44513d42069bdc2e715df0d8252a441ff5401d
Rehan Haider <email@rehanhaider.com>
2021-10-31 09:32:04 +0000

    NA

---
+++ tests/test_base.py
@@ -0,0 +1,13 @@
+"""
+# --*-- coding: utf-8 --*--
+# Path: tests/test_base.py
+# Test the base data
+"""
+import pytest
+from bs4 import BeautifulSoup
+
+baseURL = "http://localhost:8080"
+
+
+def test_pagination():
+    pass
