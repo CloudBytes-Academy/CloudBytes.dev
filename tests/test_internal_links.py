9708583af7a52aaade64221d819abac34d40d2c3
Rehan Haider <email@rehanhaider.com>
2022-09-10 18:05:22 +0000

    ARTICLE: Jupyter notebook with virtual environment

--- tests/test_internal_links.py
+++ tests/test_internal_links.py
@@ -1,10 +1,10 @@
-import pyHtmlProofer
+import pyhtmlproofer
 
 options = {"log_level": "ERROR", "disable_external": True}
 
 
 def test_internal_links2():
     directory_path = ["output"]
-    failures = pyHtmlProofer.directories(directory_path, options=options).check()
+    failures = pyhtmlproofer.directories(directory_path, options=options).check()
 
     assert len(failures) == 0
