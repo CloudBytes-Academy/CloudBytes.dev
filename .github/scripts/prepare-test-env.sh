606ae00eb3c16dad53853cbd405f2a962728ad05
Rehan Haider <email@rehanhaider.com>
2024-12-06 15:48:04 +0000

    checking what files are copied

--- .github/scripts/prepare-test-env.sh
+++ .github/scripts/prepare-test-env.sh
@@ -7,6 +7,9 @@ curl -sSL https://firebase.tools | bash
 echo "Install 🐍Python Dependencies"
 python3 -m pip install -r requirements.txt
 
+ls -al
+cat .firebaserc
+
 echo "Starting 🔥Firebase CLI"
 firebase emulators:start --only hosting &
 
